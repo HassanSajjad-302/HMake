88f4e5766a3320d848e8f667c689d344ed3b40b7
Hassan Sajjad <hassan.sajjad069@gmail.com>
2023-03-17 19:23:37 +0500

    temp commit. A little more comment added.

--- hconfigure/header/ObjectFileProducer.hpp
+++ hconfigure/header/ObjectFileProducer.hpp
@@ -21,11 +21,15 @@ struct ObjectFileProducer : public BTarget
                                 class LinkOrArchiveTarget *linkOrArchiveTarget) const = 0;
 };
 
+// This is used in conjunction with DSC to specify dependencies.
 template <typename T> struct ObjectFileProducerWithDS : public ObjectFileProducer, public DS<T>
 {
+    // Following are needed to propagate dependencies above if linkOrArchiveTarget of both DSC(dependency and dependent)
+    // is nullptr i.e. if there are 3 targets A,B,C. C being executable and have dependency on B(a library) having a
+    // dependency on A(a library). Then object-files of A would be propagated to C through linkOrArchiveTarget of B. But
+    // in-case both B and C are object-file-targets, then object-files of C will propagate through object-files of B
     set<const ObjectFileProducer *> requirementObjectFileTargets;
     set<const ObjectFileProducer *> usageRequirementObjectFileProducers;
 };
 
 #endif // HMAKE_OBJECTFILEPRODUCER_HPP
-
