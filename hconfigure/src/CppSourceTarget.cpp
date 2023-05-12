53da8d42a46847e1516c741b68195a12d545c10f
Hassan Sajjad <hassan.sajjad069@gmail.com>
2023-05-12 13:54:12 +0500

    A bug fix. Iterator wasn't being incremented in CppSourceTarget::setCompileCommand. SFML basic configuration support close to completion.

--- hconfigure/src/CppSourceTarget.cpp
+++ hconfigure/src/CppSourceTarget.cpp
@@ -1056,6 +1056,7 @@ void CppSourceTarget::setCompileCommand()
     for (; i < reqIncSizeBeforePopulate; ++i)
     {
         compileCommand.append(getIncludeFlag() + addQuotes(it->node->filePath) + " ");
+        ++it;
     }
 
     set<string> includes;
