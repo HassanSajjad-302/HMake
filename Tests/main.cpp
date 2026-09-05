1925ece7a3f3b2cec4f1f445bc08ca74c3b9db05
Hassan Sajjad <hassan.sajjad069@gmail.com>
2026-09-05 21:59:26 +0500

    t

--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,6 +1,8 @@
+#include "BuildSystemFunctions.hpp"
 #include <gtest/gtest.h>
 int main(int argc, char *argv[])
 {
+    sanitizeStandardDescriptors();
     testing::InitGoogleTest(&argc, argv);
     //::testing::GTEST_FLAG(filter) = "CAP*";
     return RUN_ALL_TESTS();
